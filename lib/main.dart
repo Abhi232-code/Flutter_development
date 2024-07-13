08d89bfe93d878b7f3db543c8be7d7377057fe87
Abhinandan-G <115647626+Abhi232-code@users.noreply.github.com>
2024-07-13 15:30:33 +0530

    Initial Setup

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+void main()=>runApp(HelloFlutter());
+class HelloFlutter extends StatelessWidget{
+  @override
+
+    
+    Widget build(BuildContext context){
+      return MaterialApp(
+        // uncomment the debug
+        debugShowCheckedModeBanner:false,
+        home:Scaffold(          //hone entire app is in Scaffold
+          backgroundColor: Colors.blue, // entire Screen Bule
+          appBar:AppBar(
+            backgroundColor: Colors.green,
+            title:const Text('Hello Flutter',
+            style: TextStyle(color:Colors.orange),
+            ),
+      
+          leading:const Icon (Icons.search),
+          actions:[
+            IconButton(
+              icon : const Icon (Icons.refresh),
+            onPressed:(){},)
+          ],
+          ),
+          body :const Center(child:Text('Hello Flutter',
+          style:TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: Colors.white),))
+            )
+          
+        
+      );
+    }
+    
+  
+    }
